96bbd0512979c1e6992d3ce2c286572ed40598fa
Edward McPherson <edward.mcpherson@yahoo.co.uk>
2025-10-02 19:36:11 +0100

    It turns out I was getting an error - I was eventually getting an error 429 for putting in too many requests when scraping data. Was quietly failing. I've added a sleep statement and a bit of error handling to get back on the road. May eventually want to remove the hard-coded sleep statement for something more respsonsive.

--- vac_ts_analysis/ingest.py
+++ vac_ts_analysis/ingest.py
@@ -1,10 +1,14 @@
 
+import time
 import requests
 from urllib.parse import urljoin
 from bs4 import BeautifulSoup
 from io import StringIO
 import pandas as pd
 from typing import Dict
+
+# Two second wait
+WAIT = 2
 
 def download_all_data(url: str, number_of_files: int = 20) -> Dict[str, pd.DataFrame]:
     """_summary_
@@ -27,12 +31,16 @@ def download_all_data(url: str, number_of_files: int = 20) -> Dict[str, pd.DataF
     for tag in csv_download_tags[:number_of_files]:
         link = tag.get('href')
         full_url = urljoin(url, link)
+        time.sleep(WAIT)
+        date = tag.get('data-gtm-date')
+        print(f"Doing: {date}")
         download_response = requests.get(full_url)
         if download_response.status_code == 200:
             # Convert CSV text to a pandas DataFrame
             csv_data = StringIO(download_response.text)
             df = pd.read_csv(csv_data)
-            date = tag.get('data-gtm-date')
             raw_data[date] = df
-    
+        else: 
+            raise requests.exceptions.RequestException("One of the requests is going wrong.")
+
     return raw_data 
